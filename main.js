0ab2f7b11164412294004f137aa595bd25214772
carlos <carloscassolfilho@gmail.com>
2023-07-13 21:23:24 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,79 @@
+// Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+// alert('Hello word!')
+
+
+
+// Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+// let number1 = 10
+// let number2 = 20
+// let result = number1 + number2
+// alert(result)
+
+
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+// let vet = '12';
+// let ifNumber = vet === Number(vet) ? 'É um número!' : 'Não é um número!'   
+// alert(ifNumber)
+
+
+
+// Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+// let vet = 1;
+// let ifString = vet === Number(vet) ? 'Não é uma String!' : 'É uma String!'   
+// alert(ifString)
+
+
+
+// Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+// let vet = false;
+// let ifString = vet === Boolean(vet) ? 'É um boolean' : 'Não é um boolean!'
+// alert(ifString)
+
+
+
+
+// Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+// let number1 = 2;
+// let number2 = 43;
+// let sub = number1 - number2
+// alert(sub)
+
+
+
+// Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// let number1 = 2;
+// let number2 = 43;
+// let mult = number1 * number2
+// alert(mult)
+
+
+
+
+
+// Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// let number1 = 2;
+// let number2 = 43;
+// let div = number1 / number2
+// alert(div.toFixed(2))
+
+
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// let getNumber = Number(prompt("Digite um número Par"))
+// if(getNumber & 0) {
+//   alert(`O número ${getNumber} é Par`)
+// } else {
+//   alert(`O número ${getNumber} é Impar`)
+// }
+
+
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+let getNumber = Number(prompt("Digite um número Impar"))
+if(getNumber & 1) {
+  alert(`O número ${getNumber} é Impar`)
+} else {
+  alert(`O número ${getNumber} é Par`)
+}
+
